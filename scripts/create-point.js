948976655bc8c5da7ebedba8dd17da2339460a96
Guglis02 <gustavomf02@gmail.com>
2020-06-04 09:08:43 -0300

    Implementação do sistema de cadastro e pesquisa

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,3 +1,5 @@
+// Dados da entidade
+
 function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
     
@@ -33,7 +35,7 @@ function getCities(event) {
     .then( cities => {
   
         for( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }      
 
         citySelect.disabled = false
@@ -43,4 +45,45 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
-    
+
+// Itens de Coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    // Add or remove class with JS
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    // Verify which items are selected
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId
+        return itemFound
+    })
+
+    if (alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    } else {
+        selectedItems.push(itemId)
+    }
+
+    collectedItems.value = selectedItems
+
+}
